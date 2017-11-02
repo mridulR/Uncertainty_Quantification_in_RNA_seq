b60a39f5cd1563ebc463705db0cc300a950ce22a
mridul <mailtoranjanmridul@gmail.com>
2017-11-02 03:46:33 -0400

    Writing valid and invalid transcripts to separate file

--- parser.py
+++ parser.py
@@ -1,14 +1,14 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-def main():
-    file_name = "poly_mo/quant_bootstraps.tsv"
-    print("Parsing quant_bootstraps.tsv file .......")
+def main(boot_strap_file, truth_value_file, show_graph):
+    print("Parsing boot_strap_file - ", boot_strap_file)
     boot_strap_exp = -1
     transcript_ids = []
     count_matrix = {}
     mean_map = {}
-    file_ = open(file_name, "r")
+    file_ = open(boot_strap_file, "r")
     for line in file_:
         boot_strap_exp += 1
         if boot_strap_exp == 0:
@@ -20,13 +20,10 @@ def main():
     print("Number of bootstrap experiments : ", boot_strap_exp)
     print("transcripts = ",len(transcript_ids))
 
-
-
-    file_name = "poly_mo/poly_truth.tsv"
     print("Parsing quant_bootstpoly_truth.tsv file .......")
 
     transcriptTrueReads={}
-    file_ = open(file_name, "r")
+    file_ = open(truth_value_file, "r")
     init=0
     for line in file_:
         
@@ -45,24 +42,36 @@ def main():
             col.append(value)
         mean_map[transcript_ids[ind]] = [np.mean(col), np.std(col)]
 
-    
+    valid_transcripts = open("valid_transcripts", "w")
+    invalid_transcripts = open("invalid_transcripts", "w")
+
     for key in mean_map.keys():
-        mu, sigma = mean_map[key] # mean and standard deviation
-        s = np.random.normal(mu, sigma, 1000)
-        count, bins, ignored = plt.hist(s, 30, normed=True)
-        plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+        if key in transcriptTrueReads:
+            mu, sigma = mean_map[key] # mean and standard deviation
+            s = np.random.normal(mu, sigma, 1000)
+            count, bins, ignored = plt.hist(s, 30, normed=True)
+            plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
                             np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
                 linewidth=1, color='r')
-        if key in transcriptTrueReads:
             meanValue = transcriptTrueReads[key]
-            plt.axvline(x=meanValue,linewidth=2, color='k')  
-        plt.axvline(x=mu+2*sigma,linewidth=2, color='g')
-        plt.axvline(x=mu-2*sigma,linewidth=2, color='g')
-        plt.show()
-    
-
-
+            if show_graph:
+                plt.axvline(x=meanValue,linewidth=2, color='k')  
+                plt.axvline(x=mu+2*sigma,linewidth=2, color='g')
+                plt.axvline(x=mu-2*sigma,linewidth=2, color='g')
+                plt.show()
+            row = key + "\t" + str(meanValue) + "\t" + str(mu - 2 * sigma) \
+                    + "\t" + str(mu) + "\t" + str(mu + 2*sigma) + "\n"    
+            if meanValue > mu - 2*sigma and meanValue < mu + 2*sigma :
+                valid_transcripts.write(row)
+            else:
+                invalid_transcripts.write(row)
     
 
 if __name__ == "__main__":
-    main()
+    boot_strap_file = "poly_mo/quant_bootstraps.tsv"
+    truth_value_file = "poly_mo/poly_truth.tsv"
+    show_graph = False
+    if len(sys.argv) == 2:
+        show_graph = True
+
+    main(boot_strap_file, truth_value_file, show_graph)
